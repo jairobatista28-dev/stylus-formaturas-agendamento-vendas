bc9cd66c931a5e772e2f03213f6b4ef2d7b8ab33
Jairo Batista <jairobatista28@gmail.com>
2026-09-13 14:06:26 -0300

    corrige leitura de datas na importacao de planilha xlsx

--- src/pages/Campaigns.tsx
+++ src/pages/Campaigns.tsx
@@ -503,7 +503,7 @@ export function Campaigns() {
 
       const sheetName = workbook.SheetNames[0];
       const worksheet = workbook.Sheets[sheetName];
-      const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 }) as string[][];
+      const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1, raw: false }) as string[][];
 
       if (!jsonData || jsonData.length < 2) {
         throw new Error('Planilha vazia ou sem cabecalho.');
